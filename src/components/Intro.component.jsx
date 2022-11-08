19c45a869baf465d484f8fe907c7c59729c50b54
herzliyaaa <herzliabarangan@gmail.com>
2022-11-08 18:29:56 +0800

    fixed stroke text; added color animation in logo

--- src/components/Intro.component.jsx
+++ src/components/Intro.component.jsx
@@ -7,20 +7,41 @@ const Intro = () => {
   return (
     <div className="flex justify-center items-center">
       <section className=" h-screen w-screen flex justify-center items-center">
-        <div className="w-screen h-screen  justify-center items-center flex flex-col">
+        <div className="justify-center items-center flex flex-col">
           <Parallax>
             <motion.img
               src={topographic}
-              animate={{ opacity: 1, filter: "white" }}
-              width={400}
-              height={400}
+              animate={{
+                opacity: 1,
+                filter: "white",
+                backgroundColor: ["#60F", "#09F", "#FA0"],
+              }}
+              transition={{
+                type: "spring",
+                duration: 1,
+                delay: 1,
+                repeatType: "reverse",
+                repeat: Infinity,
+              }}
+              width={200}
+              height={200}
+              className="object-cover object-center"
             />
           </Parallax>
           <Parallax>
             <motion.h1
-              animate={{ opacity: 1 }}
-              // animate={{ y: [0, 150, 150, 0] }}
-              // transition={{ duration: 1, repeat: Infinity }}
+              // animate={{ opacity: 1 }}
+              animate={
+                {
+                  // y: [0, 150, 150, 0],
+                }
+              }
+              transition={{
+                duration: 1,
+                delay: 1,
+                repeatType: "reverse",
+                repeat: Infinity,
+              }}
               className="text-center font-bold text-8xl stroke-text"
             >
               HELLO WORLD.
