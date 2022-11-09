a91486b8e89f56c5f5aacec5b0c5cd5527076ec7
herzliyaaa <herzliabarangan@gmail.com>
2022-11-09 17:30:00 +0800

    Intro

--- src/components/Intro.component.jsx
+++ src/components/Intro.component.jsx
@@ -5,9 +5,9 @@ import topographic from "../images/icons/topo-second.png";
 
 const Intro = () => {
   return (
-    <div className='flex justify-center items-center'>
-      <section className='h-screen flex justify-center items-center'>
-        <div className='justify-center items-center flex flex-col'>
+    <div className="flex justify-center items-center">
+      <section className="h-screen flex justify-center items-center">
+        <div className="justify-center items-center flex flex-col">
           <Parallax>
             <motion.img
               src={topographic}
@@ -25,33 +25,34 @@ const Intro = () => {
               }}
               width={200}
               height={200}
-              className='object-cover object-center'
+              alt="topographic logo"
+              className="object-cover object-center"
             />
           </Parallax>
           <Parallax>
-            <div className='flex flex-col'>
+            <div className="flex flex-col">
               <div className="flex">
-              <motion.h1
-                animate={
-                  {
-                    // y: [0, 150, 150, 0],
-                    // opacity: 1
+                <motion.h1
+                  animate={
+                    {
+                      // y: [0, 150, 150, 0],
+                      // opacity: 1
+                    }
                   }
-                }
-                transition={{
-                  duration: 1,
-                  delay: 1,
-                  repeatType: "reverse",
-                  repeat: Infinity,
-                }}
-                className='text-center font-bold xl:text-7xl lg:text-6xl sm:text-5xl xs: text-4xl stroke-text'
-              >
-                HERZLIA JANE BARANGAN,
-              </motion.h1>
+                  transition={{
+                    duration: 1,
+                    delay: 1,
+                    repeatType: "reverse",
+                    repeat: Infinity,
+                  }}
+                  className="text-center font-bold xl:text-7xl lg:text-6xl sm:text-5xl xs: text-4xl stroke-text"
+                >
+                  HERZLIA JANE BARANGAN,
+                </motion.h1>
               </div>
-            
+
               <div className="flex lg:justify-end xl:justify-end sm:justify-center xs: justify-center">
-                <p className='text-white text-right xl:text-xl lg:text-xl sm:text-xl xs: text-lg'>
+                <p className="text-white text-right xl:text-xl lg:text-xl sm:text-xl xs: text-lg">
                   your developer.
                 </p>
               </div>
